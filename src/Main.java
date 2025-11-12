2f819f899be6c0f41d711b744bc56dd558abf077
NemethBalintMark <118978756+NemethBalintMark@users.noreply.github.com>
2025-11-12 12:54:12 +0100

    comment

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        BanknoteStorage storage = new BanknoteStorage();
+        // BanknoteStorage storage = new BanknoteStorage();
 
-        storage.printStorageStatus();
-
-        System.out.println("\nHozzáadunk 5 db 2000 Ft-ost:");
-        storage.addBanknotes(2000, 5);
-
-        storage.printStorageStatus();
-
-        System.out.println("\nA 10000 Ft-osok száma: " + storage.getCountForDenomination(10000));
+//        storage.printStorageStatus();
+//
+//        System.out.println("\nHozzáadunk 5 db 2000 Ft-ost:");
+//        storage.addBanknotes(2000, 5);
+//
+//        storage.printStorageStatus();
+//
+//        System.out.println("\nA 10000 Ft-osok száma: " + storage.getCountForDenomination(10000));
     }
 }
